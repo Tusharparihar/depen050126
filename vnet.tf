eaea47c8d29f471ca1fe504354b0cd809536a6db
Tusharparihar <143849896+Tusharparihar@users.noreply.github.com>
2026-01-05 12:36:20 +0530

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,3 +1,3 @@
 module "vnet" {
-  source = "git::https://github.com/your-org/terraform-vnet-test.git?ref=v1.0.0"
+  source = "git::https://github.com/Tusharparihar/terraform-vnet-test.git?ref=v1.0.0"
 }
